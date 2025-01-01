0b49286af121aee818d418383abdad079d790d87
grajeiro123 <juniorgranjeiro.jc@gmail.com>
2025-01-01 11:53:08 -0300

    add form de login

--- assets/js/ms.js
+++ assets/js/ms.js
@@ -2,5 +2,12 @@ let carrinho = document.querySelector(".carrinho")
 
 document.querySelector("#cart").onclick = () =>{
   carrinho.classList.toggle('active');
+  login.classList.remove('active')
+}
 
+let login = document.querySelector('.login-form');
+
+document.querySelector('#login').onclick = () =>{
+  login.classList.toggle('active')
+  carrinho.classList.remove('active');
 }
